477e9ed1387a5dda4ef9e51afea9adafa652c506
Saad Ahmad <personal@saadahmad.dev>
2022-10-04 23:43:17 +1100

    Added bittom

--- frontend/components/pages/IndexPage/index.tsx
+++ frontend/components/pages/IndexPage/index.tsx
@@ -6,6 +6,7 @@ import SearchBar from "../../Inputs/SearchBar";
 import ThemedImage from "../../ThemedImage";
 import UseLocationButton from "./Buttons/UseLocation";
 import SearchButton from "./Buttons/Search";
+import Link from "next/link";
 
 
 const IndexPage = () => {
@@ -15,14 +16,13 @@ const IndexPage = () => {
     const handleOnSearch = () => {}
     return (
         <Layout nav={false}>
-            <Container className="flex md:items-center justify-center min-h-screen bg-no-repeat bg-cover"
-                       style={{backgroundImage: `url(${require("../../../assets/blue-black-material-design-8k-ui.jpeg")?.default?.src})`}}>
-                <div className="md:border md:rounded w-screen md:w-[500px] xl:w-[600px] p-6 py-16 md:p-8 bg-white shadow-lg">
+            <Container className="flex md:items-center justify-center min-h-screen bg-no-repeat bg-cover">
+                <div className="md:rounded w-screen md:w-[500px] xl:w-[600px] p-6 py-16 md:p-8 bg-white">
                     <h1 className="w-full text-4xl font-bold text-gray-700 text-center">
                         GeoHash
                     </h1>
                     <Separator />
-                    <p className={"text-gray-400 text-center"}>
+                    <p className={"text-gray-500 text-center"}>
                         This is a test project that I have developed to implement GeoHash
                         for location search services.
                     </p>
@@ -48,6 +48,11 @@ const IndexPage = () => {
                     <div className="flex items-center justify-center">
                         <p className="text-gray-500 cursor-pointer hover:underline">Create a location</p>
                     </div>
+                    <Separator padding={20}/>
+                    <p className="text-sm text-gray-400 text-center font-light">
+                        Created by Saad Ahmad. The application code, architecture and documentation
+                        can be found on <a className="underline hover:text-gray-800" href="https://github.com/SaadAhmad123/test-location-sercice-app" target={"_block"}>github</a>.
+                    </p>
                 </div>
             </Container>
         </Layout>
